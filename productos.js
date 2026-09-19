0455bf431ab2513ba269d600324c585f4d80279c
axellangarita4-ux <axellangarita4@gmail.com>
2026-09-19 12:43:58 -0400

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,26 @@
+const initialProducts = [
+    {
+        id: 1,
+        name: "Pan Canilla Tradicional",
+        category: "panaderia",
+        price: 0.80,
+        description: "Crujiente por fuera, suave y fresco por dentro.",
+        image: "https://images.unsplash.com/photo-1509440159596-0249088772ff?auto=format&fit=crop&w=600&q=80"
+    },
+    {
+        id: 2,
+        name: "Pan Sobado",
+        category: "panaderia",
+        price: 1.20,
+        description: "El auténtico sabor tradicional ligeramente dulce.",
+        image: "https://images.unsplash.com/photo-1549931319-a545dcf3bc73?auto=format&fit=crop&w=600&q=80"
+    },
+    {
+        id: 3,
+        name: "Torta Tres Leches (16 porciones)",
+        category: "pasteleria",
+        price: 15.00,
+        description: "Esponjosa torta empapada en nuestra mezcla especial de tres leches.",
+        image: "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?auto=format&fit=crop&w=600&q=80"
+    }
+];
